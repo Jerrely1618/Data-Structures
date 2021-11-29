49f83d03c9409181297bd611958bfc774c9515c5
Jerrely Vizueta <jerrely10@gmail.com>
2021-11-29 12:17:49 -0500

    November 29

--- Data Structures/Hw7_1.cpp
+++ Data Structures/Hw7_1.cpp
@@ -3,21 +3,23 @@
 //		...initializes these arrays with user - defined values
 //		...sorts the arrays by ascending value
 //		...reverses the values of the sorted arrays
-//		passes the arrays to a function Sum() that  calculates the sum of all the data elements
-//		Write a program that contains a �Dog� class whose data consists of int Id and a static int cnt that keeps track of how many dogs have been created.The Dog class should contain default constructor  that sets the Id value with the updated vale of cntand prints a message : cout << �dog #Id : � << X[i].Id.The Dog class should also have a copy constructor that updates cnt and assigns it to the Id# of constructed dog.The copy constructor should output the Id# of the constructed object.The Dog class should have a destructor that reduces the cnt value each time a �Dog� goes out of scope.The destructor should print the Id# of the �dog� that goes out of scope.
-//	
-//		Create a vector array of dogs(size 8).
-//		Output the Id values of all the dogs
-//	 After creating the Dog array, insert a new dog into location #5.
-//Explain the stream of outputs that this problem creates.
+//		...passes the arrays to a function Sum() that  calculates the sum of all the data elements
+
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
 using namespace std;
 
+template <typename T>
+void SUM(T v1) {
+	for (int x = 1; x < 8; x++) {
+		v1[0] += v1[x];
+	}
+	cout << "Sum of the vector: " << v1[0] << endl;
+}
 
-int main() {
+
+int asdmaifsdfsan() {
 	string Name;
 	/*****************************************/
 	cout << "Student Enter Name" << endl;
@@ -59,25 +61,31 @@ int main() {
 			cin >> d.second;
 			v5[y]=d;
 	}
-	
-	sort(v1.begin(),v1.end());
+	int temp(0);
+	for (int x = 0; x < 8; x++){
+		for (int y = x + 1; y < 8; y++){
+			if (v1[x] > v1[y])
+			{
+				temp = v1[x];
+				v1[x] = v1[y];
+				v1[y] = temp;
+			}
+		}
+	}
+	temp = 0;
+	for (int x = 0; x < 8; x++) {
+		for (int y = x + 1; y < 8; y++) {
+			if (v1[x] < v1[y]){
+				temp = v1[x];
+				v1[x] = v1[y];
+				v1[y] = temp;
+			}
+		}
+	}
+	SUM(v1);
 
-	sort(v2.begin(),v2.end());
-
-	sort(v3.begin(),v3.end());
-
-	sort(v4.begin(),v4.end());
-
-	sort(v5.begin(),v5.end());
-	
-	reverse(v1.begin(),v1.end());
-
-	reverse(v2.begin(),v2.end());
-
-	reverse(v3.begin(),v3.end());
-
-	reverse(v4.begin(),v4.end());
-
-	reverse(v5.begin(),v5.end());
+	for (int z = 0; z < 8; z++) {
+		cout << v1[z] << endl;
+	}
 	return 0;
 }
