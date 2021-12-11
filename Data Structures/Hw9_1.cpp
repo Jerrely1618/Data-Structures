6126ad405ca6c50beccc9c1d45a1bcc052cb6c9f
Jerrely1618 <94874872+Jerrely1618@users.noreply.github.com>
2021-12-10 22:57:44 -0500

    December 10

--- Data Structures/Hw9_1.cpp
+++ Data Structures/Hw9_1.cpp
@@ -1,7 +1,7 @@
 //?	Using the map container which contains a string for the  input wordand int cnt for the number of chars in each stored word, write an STL program that takes the following phrases from the Gettysburg Address :
 //
-//“Four scoreand seven years ago our fathers brought forth upon this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.
-//Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure.We are met on a great battle - field of that war.We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live.It is altogether fittingand proper that we should do this..”
+//ï¿½Four scoreand seven years ago our fathers brought forth upon this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.
+//Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure.We are met on a great battle - field of that war.We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live.It is altogether fittingand proper that we should do this..ï¿½
 //Output in ascending order all the words contained in this speechand their corresponding frequency  value
 //	Input a series fruit names(~20) with their corresponding weights in a multimap container.
 //Print all the fruitsand corresponding weights
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-int mainseret(int size, char* arr[]) {
+int mainseret() {
 	string Name;
 	/*****************************************/
 	cout << "Student Enter Name" << endl;
@@ -20,8 +20,6 @@ int mainseret(int size, char* arr[]) {
 	cout << "Student Name: " << Name << endl;
 	cout << "----------------------------------------------" << endl;
 	/*****************************************/
-	for (int t = 1; t < 17; ++t) {
-		cout << arr[t] << endl;
-	}
+	
 	return 0;
 }
